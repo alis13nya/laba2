d8abf37a914de13e651152e9f5f6f90febfaa242
alis13nya <166594142+alis13nya@users.noreply.github.com>
2025-04-29 18:32:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-mport { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,tailwindcss()],
-  base: "./",
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
-  }
+  plugins: [react()],
 })
-
